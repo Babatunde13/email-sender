df9f62c20a234108e27b869fe379350cfbb1d3f2
Babatunde13 <koikibabatunde14@gmail.com>
2020-09-12 20:51:47 +0100

    Initial commit

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ from flask_mail import Mail, Message
 app = Flask(__name__)
 
 app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY')
-app.config['MAIL_SERVER']='smtp.googlemail.com'
+app.config['MAIL_SERVER']=os.environ.get('MAIL_SERVER')
 app.config['MAIL_USE_TLS']=1
 app.config['MAIL_PORT']=587
 app.config['MAIL_USERNAME']=os.environ.get('MAIL_USERNAME')
@@ -13,8 +13,8 @@ app.config['MAIL_PASSWORD']=os.environ.get('MAIL_PASSWORD')
 
 mail = Mail(app)
 
-def send_mail(sender, recipients, body, **kwargs):
-    msg = Message(sender=sender, recipients=recipients, **kwargs)
+def send_mail(sender, recipients, body, subject=''):
+    msg = Message(subject=subject, sender=sender, recipients=recipients)
     msg.body=body
     mail.send(msg)
     return 'sent'
@@ -33,15 +33,27 @@ def email_me():
         return message
     else:
         pass
-    send_mail(data['sender'], data['recipients'], data['body'])
+    if data['subject']:
+        send_mail(data['sender'], data['recipients'], data['body'], data['subject'])
+    else:
+        send_mail(data['sender'], data['recipients'], data['body'])
     return jsonify(message)
 
 @app.errorhandler(500)
 def error(e):
     return jsonify({'message': 'something went wrong'}), 500
 
+@app.errorhandler(400)
+def error1(e):
+    return jsonify({'message': 'something went wrong'}), 500
+
+@app.errorhandler(405)
+def error2(e):
+    return jsonify({'message': 'Method not allowed'}), 500
+
 @app.errorhandler(404)
 def not_found(e):
     return jsonify({'message': 'Endpoint not found'}), 404
 
-app.run()
+if __name__ == "__main__":
+    app.run()
